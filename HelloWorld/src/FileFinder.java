0b7574b64fdaee78845259eb6583c1a785a71d33
keesoep kim <kissanemos@gmail.com>
2012-05-10 16:05:57 +0900

    pbl source conversion logic

--- HelloWorld/src/FileFinder.java
+++ HelloWorld/src/FileFinder.java
@@ -2,6 +2,9 @@ import java.io.*;
 
 public class FileFinder {
 	private PblFileValidator imageValidator = new PblFileValidator();
+	static String FILE_SEPARATOR   = System.getProperty("file.separator");
+	private String userDir = System.getProperty("user.dir");
+	private String workingDir;
 	
     public void listAllFiles(String path) {
         File root = new File(path);
@@ -21,28 +24,45 @@ public class FileFinder {
     public void listFilesByExport(String path) {
         File root = new File(path);
         File[] list = root.listFiles(new ImageFileFilter());
-        /*
-        try {
-        	Process oProcess = new ProcessBuilder("C://Program Files//Microsoft Office//Office12//EXCEL.exe","d:/test.xls").start();
-        } catch (IOException e) { // 에러 처리
-    	    System.err.println("에러! 외부 명령 실행에 실패했습니다.\n" + e.getMessage());
-    	    System.exit(-1);
-    	}
-    	*/
         
         if (list != null) {  // In case of access error, list is null
             for (File f : list) {
                 if (f.isDirectory()) {
                     //System.out.println(f.getAbsoluteFile());
-                    listAllFiles(f.getAbsolutePath());
+                	listFilesByExport(f.getAbsolutePath());
                 } else {
                     if ( imageValidator.validate(f.getName()) ) {
-                    	//System.out.println(f.getAbsoluteFile());
-                    	System.out.println(f.getAbsolutePath());
                     	try {
-                    		Process oProcess = new ProcessBuilder("E:\\pbldump-1.3.1stable\\pbldump.exe", "-es", "test_app.pbl","*.*").start();            	
-                    	} catch (IOException e) { // 에러 처리
-                    	    System.err.println("에러! 외부 명령 실행에 실패했습니다.\n" + e.getMessage());
+                    		String line;
+                    		String[] commands =
+                    		       {"cmd.exe",
+                    		        "/c",
+                    		        "e:"
+                    		        + FILE_SEPARATOR
+                    		        + "pbldump-1.3.1stable"
+                    		        + FILE_SEPARATOR + "pbldump.exe -es " + f.getAbsolutePath() + " *.*"};
+
+                    		Process oProcess = new ProcessBuilder(commands).start();
+                    		/*
+                    		Process oProcess = new ProcessBuilder("cmd", "/c", "dir").start();
+                    		BufferedReader bri = new BufferedReader
+                    		        (new InputStreamReader(oProcess.getInputStream()));
+                    		BufferedReader bre = new BufferedReader
+                    		        (new InputStreamReader(oProcess.getErrorStream()));
+                    		while ((line = bri.readLine()) != null) {
+                    		  System.out.println(line);
+                    		}
+                    		bri.close();
+                    		while ((line = bre.readLine()) != null) {
+                    		  System.out.println(line);
+                    		}
+                    		      bre.close();
+                    		      oProcess.waitFor();
+                    		      System.out.println("Done.");
+                    	    */
+                    	} catch (Exception err) { // 에러 처리
+                    		System.setProperty("user.dir", userDir);
+                    	    System.err.println("에러! 외부 명령 실행에 실패했습니다.\n" + err.getMessage());
                     	    System.exit(-1);
                     	}
                     }
@@ -51,7 +71,7 @@ public class FileFinder {
         }
         
     }
- 
+    /*
     public static void main(String[] args) {
         FileFinder ff = new FileFinder();
         String rootFolder = "c:\\kdac\\PBL\\PB90\\wip_execute\\";
@@ -59,4 +79,5 @@ public class FileFinder {
         System.out.println("------------------------------------");
         ff.listFilesByExport(rootFolder); // this will take a while to run!
     }
+    */
 }
