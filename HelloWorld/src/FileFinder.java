99b48eb0ca46a0403a58de5b3483de7afd839652
keesoep kim <kissanemos@gmail.com>
2012-05-11 18:18:58 +0900

    날짜 비교클래스 추가

--- HelloWorld/src/FileFinder.java
+++ HelloWorld/src/FileFinder.java
@@ -1,4 +1,6 @@
-import java.io.*;
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class FileFinder {
 	private PblFileValidator imageValidator = new PblFileValidator();
@@ -31,7 +33,12 @@ public class FileFinder {
                     //System.out.println(f.getAbsoluteFile());
                 	listFilesByExport(f.getAbsolutePath());
                 } else {
-                    if ( imageValidator.validate(f.getName()) ) {
+                	// 변경된 파일날짜 체크
+                	long curTime = new DateToday().getNightNoon();
+                	long lastTime = f.lastModified();
+                    if ( imageValidator.validate(f.getName()) && f.lastModified() > curTime ) {
+                    	System.out.println(f.getName());
+                    	/*
                     	try {
                     		String line;
                     		String[] commands =
@@ -43,7 +50,7 @@ public class FileFinder {
                     		        + FILE_SEPARATOR + "pbldump.exe -es " + f.getAbsolutePath() + " *.*"};
 
                     		Process oProcess = new ProcessBuilder(commands).start();
-                    		/*
+                    		
                     		Process oProcess = new ProcessBuilder("cmd", "/c", "dir").start();
                     		BufferedReader bri = new BufferedReader
                     		        (new InputStreamReader(oProcess.getInputStream()));
@@ -59,19 +66,20 @@ public class FileFinder {
                     		      bre.close();
                     		      oProcess.waitFor();
                     		      System.out.println("Done.");
-                    	    */
+                    	    
                     	} catch (Exception err) { // 에러 처리
                     		System.setProperty("user.dir", userDir);
                     	    System.err.println("에러! 외부 명령 실행에 실패했습니다.\n" + err.getMessage());
                     	    System.exit(-1);
                     	}
+                    	*/
                     }
                 }
             }
         }
         
     }
-    /*
+    
     public static void main(String[] args) {
         FileFinder ff = new FileFinder();
         String rootFolder = "c:\\kdac\\PBL\\PB90\\wip_execute\\";
@@ -79,5 +87,5 @@ public class FileFinder {
         System.out.println("------------------------------------");
         ff.listFilesByExport(rootFolder); // this will take a while to run!
     }
-    */
+    
 }
