fa9437906d488e11fa1cb519bc80f85d02f68590
keesoep kim <kissanemos@gmail.com>
2012-08-30 10:20:02 +0900

    delete HelloWorld, insert JdbcDriver

--- HelloWorld/src/GeneratePbldump.java
+++
@@ -1,9 +0,0 @@
-import org.apache.tools.ant.Task;
-
-public class GeneratePbldump extends Task {
-	public void execute() {
-		FileFinder ff = new FileFinder();
-        String rootFolder = "c:\\kdac\\";
-        ff.listFilesByExport(rootFolder);
-    }
-}
